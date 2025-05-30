dd2da1f1df24a8f5629edc198266e5dce3edc7ea
Danylo Piatyhorets <Danylo.Piatyhorets@city.ac.uk>
2025-05-30 22:09:11 +0100

    feat: trivial loop time complx

--- parser/go_parser.go
+++ parser/go_parser.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
@@ -10,10 +9,10 @@ import (
 )
 
 type FunctionInfo struct {
-	Name           string
-	TimeComplexity int
-	StartLine      int
-	EndLine        int
+	Name                string
+	TimeComplexityIndex int
+	StartLine           int
+	EndLine             int
 }
 
 func Process(filePath string) ([]FunctionInfo, error) {
@@ -30,22 +29,21 @@ func Process(filePath string) ([]FunctionInfo, error) {
 
 	var funcs []FunctionInfo
 
-
 	ast.Inspect(node, func(node ast.Node) bool {
 		currentFunction, isFunction := node.(*ast.FuncDecl) // this is a current node
 		if isFunction {
-            
+
 			pos := fset.Position(currentFunction.Pos())
 			end := fset.Position(currentFunction.End())
-			
-            var funcStmtList []ast.Stmt = currentFunction.Body.List
-            var timeComplexityIndex int = getMaxLoopDepth(funcStmtList, 0, 0)
 
-            funcs = append(funcs, FunctionInfo{
-				Name:      currentFunction.Name.Name,
-                TimeComplexity: timeComplexityIndex,
-				StartLine: pos.Line,
-				EndLine:   end.Line,
+			var funcStmtList []ast.Stmt = currentFunction.Body.List
+			var timeComplexityIndex int = getMaxLoopDepth(funcStmtList, 0, 0)
+
+			funcs = append(funcs, FunctionInfo{
+				Name:                currentFunction.Name.Name,
+				TimeComplexityIndex: timeComplexityIndex,
+				StartLine:           pos.Line,
+				EndLine:             end.Line,
 			})
 		}
 		return true
@@ -54,18 +52,30 @@ func Process(filePath string) ([]FunctionInfo, error) {
 	return funcs, nil
 }
 
-func getMaxLoopDepth(currentBodyList []ast.Stmt, maxDepth int , currentDepth int) int {
-    maxDepth = int(math.Max(float64(currentDepth), float64(maxDepth)))
-	fset := token.NewFileSet()
-    fmt.Print("For current List currentDepth: ", currentDepth, " maxDepth: ", maxDepth)
-    for _, stmt := range currentBodyList {
-	    ast.Print(fset, stmt)
-    }
-    for _, forStmt := range currentBodyList{
-        currentLoop, isLoop := forStmt.(*ast.ForStmt)
-        if isLoop {
-            return getMaxLoopDepth(currentLoop.Body.List, maxDepth, currentDepth+1)
-        }
-    }
-    return maxDepth
+func getMaxLoopDepth(currentBodyList []ast.Stmt, maxDepth int, currentDepth int) int {
+	maxDepth = int(math.Max(float64(currentDepth), float64(maxDepth)))
+	for _, stmt := range currentBodyList {
+		switch stmtType := stmt.(type) {
+		case *ast.ForStmt:
+			maxDepth = getMaxLoopDepth(stmtType.Body.List, maxDepth, currentDepth+1)
+		case *ast.RangeStmt:
+			maxDepth = getMaxLoopDepth(stmtType.Body.List, maxDepth, currentDepth+1)
+		case *ast.LabeledStmt:
+			maxDepth = getMaxLoopDepth([]ast.Stmt{stmtType.Stmt}, maxDepth, currentDepth)
+		case *ast.IfStmt:
+			maxDepth = getMaxLoopDepth(stmtType.Body.List, maxDepth, currentDepth) // go deeper without incrementing
+			if stmtType.Else != nil {
+				if elseBlock, ok := stmtType.Else.(*ast.BlockStmt); ok {
+					maxDepth = getMaxLoopDepth(elseBlock.List, maxDepth, currentDepth)
+				}
+			}
+		case *ast.SwitchStmt:
+			for _, stmt := range stmtType.Body.List {
+				if caseClause, isCaseClause := stmt.(*ast.CaseClause); isCaseClause {
+					maxDepth = getMaxLoopDepth(caseClause.Body, maxDepth, currentDepth)
+				}
+			}
+		}
+	}
+	return maxDepth
 }
--- test/test.go
+++ test/test.go
@@ -8,9 +8,27 @@ import (
 )
 
 func main() {
+    // src := `
+    // package main
+    // import "fmt"
+
+    // func greet(name string) string {
+    //     return "Hello, " + name
+    // }
+
+    // func haveLoop() {
+    //     for i := 0; i < 10; i++ {
+    //         fmt.Println(i)
+    //         for j := 0; j < 10; j++ {
+    //             fmt.Println(j)
+    //         }
+    //     }
+    // }    
+    // `
     src := `
     package main
-    import "fmt"
+
+import "fmt"
 
     func greet(name string) string {
         return "Hello, " + name
@@ -25,6 +43,7 @@ func main() {
         }
     }    
     `
+    
 
     fset := token.NewFileSet()
     file, err := parser.ParseFile(fset, "", src, parser.AllErrors)
